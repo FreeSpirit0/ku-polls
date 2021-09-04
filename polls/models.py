96e34f243d5d4952ef4f6643c78eecbaa4462525
Poomtum <poomtum2544@hotmail.com>
2021-09-04 16:20:34 +0700

    :hammer: Add template for admin page #12

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,18 @@
 import datetime
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length = 200)
+    question_text = models.CharField(max_length=200)
     publication_date = models.DateTimeField('date published')
 
+    @admin.display(
+        boolean=True,
+        ordering='publication_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.publication_date <= now
@@ -16,9 +22,9 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
-    choice_text = models.CharField(max_length = 200)
-    votes = models.IntegerField(default = 0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
